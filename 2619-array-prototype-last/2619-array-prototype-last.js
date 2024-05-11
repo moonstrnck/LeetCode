d92c63acf827d453ea687defd3f8061e13729259
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-05-11 13:32:07 +0900

    Time: 61 ms (7.21%), Space: 49.4 MB (6.45%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,10 +2,10 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if (this[this.length - 1] !== undefined) {
-        return this[this.length - 1];
-    } else {
+    if (this.length === 0) {
         return -1;
+    } else {
+        return this[this.length - 1];
     }
 };
 
