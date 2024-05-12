903c613f58b4cd1a8d1192275591cf515cc6ba78
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-05-12 10:43:42 +0900

    Time: 62 ms (62.42%), Space: 50.2 MB (54.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+const twoSum = function (nums, target) {
+    const numIndices = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const difference = target - nums[i];
+        if (difference in numIndices) {
+            return [numIndices[difference], i];
         }
+        numIndices[nums[i]] = i;
     }
 };
