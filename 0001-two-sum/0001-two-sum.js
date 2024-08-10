081342876368349f5e0c4bd3f696d78e1b2b2c2a
Seunghwi Moon <81969210+moonstrnck@users.noreply.github.com>
2024-08-10 21:38:44 +0900

    Time: 94 ms (43.92%), Space: 49.6 MB (73.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,14 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function (nums, target) {
-    const numIndices = {};
 
-    for (let i = 0; i < nums.length; i++) {
-        const difference = target - nums[i];
-        if (difference in numIndices) {
-            return [numIndices[difference], i];
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result = [i, j];
+                
+                return [i, j];
+            }
         }
-        numIndices[nums[i]] = i;
     }
 };
