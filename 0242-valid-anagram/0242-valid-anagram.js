44851ffb0f45ec26aacf21abf41e6c28a71c0d64
Seunghwi Moon <81969210+moonstrnck@users.noreply.github.com>
2024-08-18 13:45:06 +0900

    Time: 83 ms (22.93%), Space: 53.4 MB (48.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    };
+    
+    const sortedS = [...s].sort().join('');
+    const sortedT = [...t].sort().join('');
+    
+    return sortedS === sortedT;
+};
