c6dd7e1abb520a06acea25660f31a0fef61d2231
Seunghwi Moon <81969210+moonstrnck@users.noreply.github.com>
2024-08-28 09:07:21 +0900

    Time: 75 ms (18.50%), Space: 54.5 MB (17.04%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,17 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let p1 = head
-    let p2 = head
+    let p1 = head;
+    let p2 = head;
     
     while (p2 && p2.next && p2.next.next) {
-        p1 = p1.next
-        p2 = p2.next.next
+        p1 = p1.next;
+        p2 = p2.next.next;
         
         if (p1 === p2) {
-            return true
+            return true;
         }
     }
     
-    return false
+    return false;
 };
